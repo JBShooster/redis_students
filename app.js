0f8c7a0ce2ced06ca270fbd76b0ac0236e322163
Joel Shooster <JBShooster@gmail.com>
2015-03-19 09:21:21 -0700

    HW sent

--- app.js
+++ app.js
@@ -12,3 +12,34 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 //If I want to include css/js/imgs
 app.use(express.static(__dirname + '/public'));
+
+app.get("/", function(req, res){
+  // use the redis client to get the students
+  client.lrange("students", 0, -1, function(err, students){
+    res.render("index", {students: students});
+  });
+});
+
+//post to create a todo
+app.post("/create", function(req, res){
+  console.log(req.body);
+  // push req.body.pupils into the students list
+  client.lpush("students",req.body.pupil);
+  //do some creating
+  res.redirect("/");
+});
+
+app.delete("/remove/:student", function(req, res){
+  client.lrange("students", 0, 1, function(err,students){
+    students.forEach(function(student){
+    if(req.params.student === student){
+      client.lrem("students", 1, student);
+      res.redirect("/");
+      }
+    });
+  });
+});
+
+app.listen(3000, function(){
+  console.log("Server is doing it and doing it and doing it well on port 3000");
+});
